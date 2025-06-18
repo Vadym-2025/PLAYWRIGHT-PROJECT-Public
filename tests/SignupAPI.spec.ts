879ba6e198c13c46e11b67aba3e60fb8d3d9d96e
Vadym Yurchenko <yurchenko.vad@gmail.com>
2025-06-18 15:44:38 +0300

    api sign un not fixed

--- tests/SignupAPI.spec.ts
+++ tests/SignupAPI.spec.ts
@@ -5,11 +5,6 @@ test('Create user via GitLab API using helper', async () => {
   const user = await createGitLabUser();
 
   console.log('✅ Created user:', user);
-  
-  // Example: navigate and log in with the user
-  // await page.goto('https://gitlab.example.com/users/sign_in');
-  // await page.fill('input[name="user[login]"]', user.email);
-  // await page.fill('input[name="user[password]"]', user.password);
-  // await page.click('input[name="commit"]');
-  // await expect(page.locator('text=Dashboard')).toBeVisible();
+
+
 });
